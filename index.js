05c40fa7eb62ceb566dcd217de17097866c66c45
Angela Shirao <angela.shirao@student.moringaschool.com>
2023-10-07 12:46:32 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  let ride
+  if(ride <= 400){ 
+return 'This one is on me!';
+  } else if(2000<ride && ride <2500){
+    return 'I will gladly take your thirty bucks.';
+  } else { 
+return 'No can do.';
+  }
 }
 
 function ternaryCheckCity(){
